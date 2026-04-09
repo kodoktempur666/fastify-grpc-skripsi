9397a9a8a1a1037156d420f1a149a1ca90bcccde
Firman Maulana <lontopcx12@gmail.com>
2026-04-10 01:57:24 +0700

    fix db config

--- fastify-api/src/config/grpc.js
+++ fastify-api/src/config/grpc.js
@@ -9,7 +9,7 @@ const packageDef = protoLoader.loadSync(
 const proto = grpc.loadPackageDefinition(packageDef).checkout;
 
 const client = new proto.CheckoutService(
-  "grpc_server:50051",
+  "127.0.0.1:50051",
   grpc.credentials.createInsecure()
 );
 
